64233a38d25e1a5e87a53864d45e5ad960f4a466
Behera Manav <manavbehera123@gmail.com>
2025-05-25 21:50:14 +0530

    created request mapping

--- src/main/java/com/example/journalApp/Controller/JournalEntryController.java
+++ src/main/java/com/example/journalApp/Controller/JournalEntryController.java
@@ -1,8 +1,27 @@
 package com.example.journalApp.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.journalApp.Entity.JournalEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
+    private Map<Long, JournalEntity> journalData = new HashMap<>();
+
+    @GetMapping("/home")
+    public List<JournalEntity> getAll(){
+        return new ArrayList<>(journalData.values());
+    }
+
+    @PostMapping
+    public  boolean createEntry(@RequestBody JournalEntity createData){
+        journalData.put(createData.getId(),createData);
+        return true;
+    }
 
 }
